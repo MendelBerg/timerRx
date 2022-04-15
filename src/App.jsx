50b4cdfcfad5c58f86ba2a25897a13eeef709098
Alexander <mendelberg44@gmail.com>
2022-04-15 10:12:46 +0200

    done with bags

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { interval } from "rxjs";
+import { map } from "rxjs/operators";
 import { Timer } from "./Timer.jsx";
 
-const observable = interval(100);
-
-const subscribe = (foo) =>
-  observable.subscribe({
-    next(x) {
-      foo(x);
-    },
-    error(err) {
-      console.error("something wrong occurred: " + err);
-    },
-    complete() {
-      console.log("done");
-    },
-  });
-
+const observable = interval(10).pipe(map((seconds) => seconds + 1));
 let subscription = null;
 
 const App = () => {
   const [seconds, setSeconds] = useState(0);
+  const [withWait, setWithWait] = useState(false);
+  const [waitClick, setWaitClick] = useState(new Date());
 
-  const start = () => {
-    if (subscription !== null) return;
+  const start = (withReset = false) => {
+    subscription = observable.subscribe({
+      next: (x) => setSeconds(withWait && !withReset ? seconds + x : x),
+      error: (err) => console.error("something wrong occurred: " + err),
+      complete: () => console.log("done"),
+    });
 
-    subscription = subscribe(setSeconds);
+    if (withWait) setWithWait(false);
   };
 
   const stop = () => {
-    if (subscription === null) return;
+    if (subscription !== null) {
+      subscription.complete();
+      subscription = null;
+    }
 
-    subscription.complete();
-    subscription = null;
+    if (!withWait) setSeconds(0);
   };
 
   const reset = () => {
     stop();
-    start();
+    start(true);
   };
 
-  const wait = () => {};
+  const wait = () => {
+    const newClick = new Date();
+
+    if (newClick - waitClick < 300 && !withWait) setWithWait(true);
+
+    setWaitClick(newClick);
+  };
+
+  useEffect(() => {
+    if (withWait) stop();
+  }, [withWait]);
 
   return (
     <Timer
+      isSubscription={subscription !== null && !withWait}
       seconds={seconds}
       start={start}
       stop={stop}
--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 
-export const Timer = ({ seconds, start, stop, reset, wait }) => (
-  <div>
-    <button onClick={start}>start</button>
-    <button onClick={stop}>stop</button>
-    <button onClick={reset}>reset</button>
-    <button onClick={wait}>wait</button>
+const getTime = (seconds) => {
+  const time = new Date(seconds * 1000);
 
-    <div>{seconds}</div>
-  </div>
-);
+  return [time.getHours() - 1, time.getMinutes(), time.getSeconds()]
+    .map((time) => (time < 10 ? `0${time}` : time))
+    .join(":");
+};
+
+export const Timer = ({
+  isSubscription,
+  seconds,
+  start,
+  stop,
+  reset,
+  wait,
+}) => {
+  return (
+    <div>
+      <button onClick={isSubscription ? stop : start}>
+        {isSubscription ? "stop" : "start"}
+      </button>
+      <button onClick={reset}>reset</button>
+      <button onClick={wait}>wait</button>
+
+      <div>{getTime(seconds)}</div>
+    </div>
+  );
+};
